f97066aac51132ee1c029692ce68001d0d203d67
amhrn <mhrn77@gmail.com>
2025-11-13 16:26:39 +0500

    fix(Ward): form to use Select for type, model fillable attributes

--- app/Filament/Resources/Wards/Schemas/WardForm.php
+++ app/Filament/Resources/Wards/Schemas/WardForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Wards\Schemas;
 
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
@@ -13,7 +14,11 @@ class WardForm
             ->components([
                 TextInput::make('name')
                     ->required(),
-                TextInput::make('type')
+                Select::make('type')
+                    ->options([
+                        'Male' => 'Male',
+                        'Female' => 'Female',
+                    ])
                     ->required(),
                 TextInput::make('capacity')
                     ->required()
