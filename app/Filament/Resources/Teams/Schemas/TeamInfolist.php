dce734efe4e8d9a5329cb52419d7539dd4dc78a0
amhrn <mhrn77@gmail.com>
2025-11-13 20:23:15 +0500

    feat: implement Team resource with create, edit, list, view pages, and schema configurations

---
+++ app/Filament/Resources/Teams/Schemas/TeamInfolist.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Teams\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class TeamInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('name'),
+                TextEntry::make('code'),
+                TextEntry::make('consultant_id')
+                    ->numeric(),
+                TextEntry::make('junior_id')
+                    ->numeric(),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+            ]);
+    }
+}
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
+    public function consultant()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 
+    public function junior()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 }
