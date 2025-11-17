09c0455054f1e666c6bb5900b69d1b24db777312
amhrn <mhrn77@gmail.com>
2025-11-17 18:40:45 +0500

    setup: laravel-permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,14 @@ use App\Models\Concerns\LogsActivityEvents;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    use HasRoles;
     use LogsActivityEvents;
 
     /**
