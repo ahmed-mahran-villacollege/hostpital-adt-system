fd7df08c09f20d5ec16ff8fa8d0b24179e5c7ed8
amhrn <mhrn77@gmail.com>
2025-11-17 15:09:46 +0500

    feat(Team): handle foreign key constraint failure during delete

--- app/Filament/Resources/Teams/Pages/EditTeam.php
+++ app/Filament/Resources/Teams/Pages/EditTeam.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Resources\Teams\Pages;
 
 use App\Filament\Resources\Teams\TeamResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
+use Illuminate\Database\QueryException;
 
 class EditTeam extends EditRecord
 {
@@ -15,7 +18,23 @@ class EditTeam extends EditRecord
     {
         return [
             ViewAction::make(),
-            DeleteAction::make(),
+            DeleteAction::make()
+                ->action(function (Action $action) {
+                    try {
+                        $this->callHook('beforeDelete');
+                        $this->getRecord()->delete();
+                        $this->callHook('afterDelete');
+                        $action->success();
+                    } catch (QueryException $exception) {
+                        Notification::make()
+                            ->danger()
+                            ->title('Cannot delete team')
+                            ->body('This team has linked patients and cannot be deleted.')
+                            ->send();
+
+                        $action->failure();
+                    }
+                }),
         ];
     }
 }
