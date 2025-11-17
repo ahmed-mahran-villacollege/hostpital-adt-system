ee03e254a734306b255ef3895c1a1a42ff9016d5
amhrn <mhrn77@gmail.com>
2025-11-17 15:46:31 +0500

    feat(Ward): handle foreign key constraint during delete

--- app/Filament/Resources/Wards/Pages/EditWard.php
+++ app/Filament/Resources/Wards/Pages/EditWard.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Resources\Wards\Pages;
 
 use App\Filament\Resources\Wards\WardResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\QueryException;
 
 class EditWard extends EditRecord
 {
@@ -15,7 +18,23 @@ class EditWard extends EditRecord
     {
         return [
             ViewAction::make(),
-            DeleteAction::make(),
+            DeleteAction::make()
+                ->action(function (Action $action) {
+                    try {
+                        $this->callHook('beforeDelete');
+                        $this->getRecord()->delete();
+                        $this->callHook('afterDelete');
+                        $action->success();
+                    } catch (QueryException $exception) {
+                        Notification::make()
+                            ->danger()
+                            ->title('Cannot delete ward')
+                            ->body('This ward has admitted patients and cannot be deleted.')
+                            ->send();
+
+                        $action->failure();
+                    }
+                }),
         ];
     }
 }
